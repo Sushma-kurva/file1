c8196404b34f67a088b1de0991a8fa0dfb59e7ef
Sushma-kurva <shusmashusma69@gmail.com>
2025-02-22 20:09:40 +0530

    assign

---
+++ assignment.py
@@ -0,0 +1,22 @@
+#date 13_02_2025
+
+'''
+Kalam was elected as the president of India in 2002 with the support of both the ruling Bharatiya Janata Party 
+
+and the then-opposition Indian National Congress. He was widely referred to as the "People's President". 
+He engaged in teaching, writing and public service after his presidency. 
+He was a recipient of several awards, including the Bharat Ratna, India's highest civilian honour.
+'''
+# fullname=input("enter the fullname:")
+# presidentyear=input("enter the year:")
+# print(f"full name is {fullname}\n presindentyear is {presidentyear}")
+
+'''
+Telugu cinema, also known as Tollywood, is the segment of Indian cinema dedicated
+ to the production of motion pictures in the Telugu language,
+ widely spoken in the states of Andhra Pradesh and Telangana. Based in Film Nagar,
+ Hyderabad, Telugu cinema is the largest film industry in India by box-office revenue as of 2021
+'''
+telugucinema=input("enter the telugucinema:")
+spokenstate=input("enter the spokenstate:")
+print(f"telugu cinema is {telugucinema}\n spoken state is {spokenstate}")
